5108fe4a8ee7ba28968274bc9e3468b13968fa7a
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-03-05 22:37:35 +0530

    module in nodejs

---
+++ nodejsModule/globalsInNode.js
@@ -0,0 +1,13 @@
+// Globals in Nodejs
+
+// 1. __dirname -> it will give you the dir path of the curr module.
+
+console.log(__dirname);
+
+// 2. require -> it is a function, this can help you to import code from other files.
+
+console.log(require);
+
+//3. process -> it is an object, it gives you information about the current process.
+
+console.log(process)
